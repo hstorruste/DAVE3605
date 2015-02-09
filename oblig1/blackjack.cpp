06baba7a98d4a3b8a580efe32d9c80b3c999edaf
Hallbjørn <s165519@stud.hioa.no>
2015-02-09 05:42:04 +0100

    Lagt til funksjon for antall poeng på hånd

--- oblig1/blackjack.cpp
+++ oblig1/blackjack.cpp
@@ -1,8 +1,33 @@
 #include "Kortstokk.hpp"
 #include "Kort.hpp"
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+int score( vector<Kort>* hand_){
+  vector<Kort> hand = *hand_;
+  //Regner sammen poeng på en hånd
+  int sum{0};
+  int aces{0}; //Antall ess
+  for(Kort k : hand){
+    int temp = k.getvalue();
+    if(temp == 1){  // Sjekker om det er et ess
+      aces++;
+      temp = 11;
+    }
+    else if(temp > 10)  //Setter verdi til 10 om det er bildekort
+      temp = 10;
+    sum += temp;
+    cout << k.toString() << endl;
+  }
+  while(sum > 21 && aces > 0){ //Setter ess til å være 1 om summen er over 21
+    sum = sum - 10;
+    aces--;
+  }
+  return sum;
+
+}
 
 int main(){
 
@@ -20,15 +45,20 @@ int main(){
 
   int antall2{0};
   stokk.stokk();
-  while(!stokk.empty()){
+  /*while(!stokk.empty()){
   Kort l = stokk.del();
   antall2++;
   cout << "Delte: " << l.toString() << endl;
-  }
+  }*/
 
   cout << "Delte første gang totalt: " << antall << " kort" <<endl;
-  cout << "Delte andre gang totalt: " << antall2 << " kort" <<endl;
-
+  //cout << "Delte andre gang totalt: " << antall2 << " kort" <<endl;
+  vector<Kort> hand{};
+  Kort m = stokk.del();
+  hand.push_back(m);
+  hand.push_back(stokk.del());
+  antall2 = score( &hand );
+  cout << "Sum på hånd: " << antall2 << endl;
 
   //  Kort m = stokk.del();
   //  cout << "Delte: " << m.getsuit() << m.getvalue() << endl;
